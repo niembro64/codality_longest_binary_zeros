683545d4ba883daa75510aabf65fe45619722a56
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-05-31 19:17:50 -0400

    asdf

--- codality_longest_binary_zeros.js
+++ codality_longest_binary_zeros.js
@@ -5,16 +5,15 @@ function dec2bin(dec) {
 }
 function solution(N) {
     // write your code in JavaScript (Node.js 8.9.4)
-    var n_string = dec2bin(N)
+    var n_string = dec2bin(N);
     var max_length = 0;
     var one_length = 0;
 
     for (let i = 0; i < n_string.length; i++) {
         if (n_string[i] == "1") {
-            max_length = (max_length < one_length ? one_length : max_length)
+            max_length = max_length < one_length ? one_length : max_length;
             one_length = 0;
         } else {
-
             one_length++;
         }
     }
